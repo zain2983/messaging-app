e7813bc4d18269e3e0e770e6770dbd6f934f68f6
zain2983 <zainijaz2983@gmail.com>
2024-11-09 21:35:18 +0500

    search functionality implemented

--- frontend/src/services/firebaseConfig.js
+++ frontend/src/services/firebaseConfig.js
@@ -1,11 +1,37 @@
+// src/services/firebaseConfig.js
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-};
+    apiKey: "AIzaSyBehKuVN6TuxnDc2fTtbQPIrFhZDTSJ5kQ",
+    authDomain: "messaging-app-128f0.firebaseapp.com",
+    projectId: "messaging-app-128f0",
+    storageBucket: "messaging-app-128f0.appspot.com",
+    messagingSenderId: "879699192787",
+    appId: "1:879699192787:web:30924ec1c8c95861115d26",
+    measurementId: "G-ZY8Y4534ZT"
+  };
+  
+  export default firebaseConfig;
+  
+  
 
-export default firebaseConfig;
+
+  // const firebaseConfig = {
+  //   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  //   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  //   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  //   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  //   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  //   appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  //   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  // };
+  
+  // export default firebaseConfig;
+  
+  
+  // REACT_APP_FIREBASE_API_KEY=AIzaSyBehKuVN6TuxnDc2fTtbQPIrFhZDTSJ5kQ
+  // REACT_APP_FIREBASE_AUTH_DOMAIN=messaging-app-128f0.firebaseapp.com
+  // REACT_APP_FIREBASE_PROJECT_ID=messaging-app-128f0
+  // REACT_APP_FIREBASE_STORAGE_BUCKET=messaging-app-128f0.appspot.com
+  // REACT_APP_FIREBASE_MESSAGING_SENDER_ID=879699192787
+  // REACT_APP_FIREBASE_APP_ID=1:879699192787:web:30924ec1c8c95861115d26
+  // REACT_APP_FIREBASE_MEASUREMENT_ID=G-ZY8Y4534ZT
+  
