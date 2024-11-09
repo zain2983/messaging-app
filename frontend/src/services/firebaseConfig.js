edc388fe1d2cfd212f515e2c9be339f8bff6c73c
zain2983 <zainijaz2983@gmail.com>
2024-11-09 21:32:53 +0500

    search functionality implemented

--- frontend/src/services/firebaseConfig.js
+++ frontend/src/services/firebaseConfig.js
@@ -1,13 +1,11 @@
-// src/services/firebaseConfig.js
 const firebaseConfig = {
-    apiKey: "AIzaSyBehKuVN6TuxnDc2fTtbQPIrFhZDTSJ5kQ",
-    authDomain: "messaging-app-128f0.firebaseapp.com",
-    projectId: "messaging-app-128f0",
-    storageBucket: "messaging-app-128f0.appspot.com",
-    messagingSenderId: "879699192787",
-    appId: "1:879699192787:web:30924ec1c8c95861115d26",
-    measurementId: "G-ZY8Y4534ZT"
-  };
-  
-  export default firebaseConfig;
-  
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
+
+export default firebaseConfig;
